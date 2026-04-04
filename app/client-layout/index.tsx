86cbe4c1e05c7d8bafadaf5fab56b3c894490117
leSchb <kurultae@yandex.ru>
2026-04-05 01:27:42 +0300

    add vercel speed insights

--- app/client-layout/index.tsx
+++ app/client-layout/index.tsx
@@ -1,9 +1,15 @@
 import StoreProvider from "../store/provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function ClientRootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <StoreProvider>{children}</StoreProvider>;
+  return (
+    <>
+      <SpeedInsights />
+      <StoreProvider>{children}</StoreProvider>
+    </>
+  );
 }
