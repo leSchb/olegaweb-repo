a25275e6c86b3c9d8c56a4438f7db22784d4204e
leSchb <kurultae@yandex.ru>
2026-04-05 18:45:26 +0300

    add og meta tags

--- shared/config/meta/layout.meta.ts
+++ shared/config/meta/layout.meta.ts
@@ -4,4 +4,14 @@ export const layoutMetadata: Metadata = {
   title: "OlegaWeb",
   description:
     "Продуктовый UX/UI-дизайнер. Делаю UX/UI для продуктов и сервисов без духоты и бесконечных правок",
+  openGraph: {
+    title: "OlegaWeb",
+    description:
+      "Продуктовый UX/UI-дизайнер. Делаю UX/UI для продуктов и сервисов без духоты и бесконечных правок",
+    images: [
+      "https://image2url.com/r2/default/images/1775403881070-823209cd-747a-450c-b48a-023a03698c3a.png",
+    ],
+    type: "website",
+    url: "https://olegaweb-repo.vercel.app",
+  },
 };
